6c879c278ee096dc20d9cd6d2f66a4ba43a16de7
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-05 02:18:35 +0530

    func

--- essential_js/array.js
+++ essential_js/array.js
@@ -1,9 +1,16 @@
-let arrays=["apple","banana","carrot","da"];
-console.log(arrays);
-console.log(arrays.length);
-arrays.pop()
-console.log(arrays)
-arrays.push("bana")
-console.log(arrays)
-arrays.unshift("ap")
-console.log(arrays)
+// let arrays=["apple","banana","carrot","da"];
+// console.log(arrays);
+// console.log(arrays.length);
+// arrays.pop()
+// console.log(arrays)
+// arrays.push("bana")
+// console.log(arrays)
+// arrays.unshift("ap")
+// console.log(arrays)
+
+// function
+function hello(name){
+    console.log("hello",name);
+}
+hello("teja");
+hello("ram")
