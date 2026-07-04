10ffa9537926d1364deab24dfdcba4f9b05f9241
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-05 02:01:10 +0530

    array

--- essential_js/array.js
+++ essential_js/array.js
@@ -1,2 +1,9 @@
-array=["apple","banana","carrot","da"];
-console.log(array(1));
+let arrays=["apple","banana","carrot","da"];
+console.log(arrays);
+console.log(arrays.length);
+arrays.pop()
+console.log(arrays)
+arrays.push("bana")
+console.log(arrays)
+arrays.unshift("ap")
+console.log(arrays)
