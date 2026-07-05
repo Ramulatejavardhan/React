9a533fa58f7da60daca1c0f92731f7c84b617b7c
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-05 20:44:34 +0530

    func

--- essential_js/array.js
+++ essential_js/array.js
@@ -9,8 +9,14 @@
 // console.log(arrays)
 
 // function
-function hello(name){
-    console.log("hello",name);
-}
-hello("teja");
-hello("ram")
+// function hello(name){
+//     console.log(`hello ${name}`);
+// }
+// hello("teja");
+// hello("ram")
+
+//arrow function
+const hello=(name,age)=>{console.log(`my name is ${name}`)
+                        console.log(`my age is ${age}`)};
+
+hello("tej",19);
