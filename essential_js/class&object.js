2009f2d2ceb3fdcae5bd37e38d36ab66ae230649
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-06 14:57:03 +0530

    class%obj

---
+++ essential_js/class&object.js
@@ -0,0 +1,16 @@
+class human{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    talk(){
+        console.log(`hello my name is ${this.name}`);
+        console.log(`my age is ${this.age}`);
+    }
+    walk(){
+        console.log(`i am going for a walk at my age ${this.age}`);
+    }
+}
+const person1=new human("teja",20);
+person1.talk();
+person1.walk()
