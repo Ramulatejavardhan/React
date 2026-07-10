df8ad02e0b51080aa5c2f37420f922177d2ff45a
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-10 19:37:48 +0530

    firstreact

---
+++ React/my-react/src/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+        <h1>hello</h1>
+        <ol>
+            <li>hello</li>
+            <li>hello</li>
+            <li>hello</li>
+        </ol>
+    </header>
+  );
+}
+export default Header;
