e5579d47504835be32683394ea6da63845ddb48f
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-15 22:38:45 +0530

    react components

--- React/my-react/src/App.jsx
+++ React/my-react/src/App.jsx
@@ -1,9 +1,11 @@
-import Component from "./component";
+import Component from "./components/component";
+import Component2 from "./components/component2";
 function App() {
   return (
     <div class="in">
     <h1>hello</h1>
     <Component/>
+    <Component2/>
     </div>
   );
 }
---
+++ React/my-react/src/components/component2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Component2(){
+    return(
+        <div class="comp2">
+            <h2>this is component2</h2>
+        </div>
+    );
+}
+export default Component2;
