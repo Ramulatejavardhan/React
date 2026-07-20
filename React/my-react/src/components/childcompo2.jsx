2ba5c431679690363f4c761442a16abde0390cf6
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-20 19:51:07 +0530

    childofcompo

--- React/my-react/src/components/childcompo2.jsx
+++ React/my-react/src/components/childcompo2.jsx
@@ -1,1 +1,9 @@
-import 
+import React from "react";
+function Childcompo2(){
+    return(
+        <section>
+            <h3>childcompo2</h3>
+        </section>
+    );
+}
+export default Childcompo2;
--- React/my-react/src/components/component2.jsx
+++ React/my-react/src/components/component2.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Childcompo2 from "./childcompo2";
 function Component2(){
     return(
         <div class="comp2">
             <h2>this is component2</h2>
+            <Childcompo2/>
         </div>
     );
 }
