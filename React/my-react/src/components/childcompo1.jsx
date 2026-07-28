a7fdc8a71b5ed928de163e4cc9480a170e74ad0f
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-28 21:17:58 +0530

    rea

--- React/my-react/src/components/childcompo1.jsx
+++ React/my-react/src/components/childcompo1.jsx
@@ -3,6 +3,7 @@ function Childcompo1(){
     return(
         <div>
             <h3 class="child">Childcompo1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi in, vero dolorem distinctio molestiae, eaque voluptas blanditiis optio nesciunt inventore quibusdam veritatis possimus cumque vitae quod natus voluptates, delectus maiores?</p>
         </div>
     );
 }
