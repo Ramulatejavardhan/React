d06f6e148e2b272d627aaa8933982ea90e664a53
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-01 16:30:36 +0530

    component

---
+++ React/react/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import teammembers from './component/teammembers'
+import Members from './component/teammembers'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>hello my details are under given:</h1>
+      <Members/>
+    </>
+  );
+}
+
+export default App
---
+++ React/react/src/component/teammembers.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Members(){
+    return(
+        <>
+        <h4>name:teja</h4>
+        <h4>role:developer</h4>
+        </>
+    );
+}
+export default Members;
