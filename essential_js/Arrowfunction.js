2dd021911d91a095e5b988bed553ce1f79e7759a
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-10 16:00:33 +0530

    arrowfunction

--- essential_js/Arrowfunction.js
+++ essential_js/Arrowfunction.js
@@ -3,4 +3,10 @@ const add=(a,b)=>{
 }
 document.getElementById("result").textContent=add(2,3)
 const greet=document.getElementById("test");
+const age = document.getElementById("age");
+const button = document.getElementById("click");
+const result = document.getElementById("eligible");
 greet.textContent="hello r";
+button.addEventListener("click",()=>{
+const adult= age>=18 ? "ur are eligble":"ur no eligible"
+result.textContent(adult)
