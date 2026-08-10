07210251484772b0672906f998bb213823e2d503
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-10 16:10:27 +0530

    arrowfunction

--- essential_js/Arrowfunction.js
+++ essential_js/Arrowfunction.js
@@ -1,12 +1,11 @@
 const add=(a,b)=>{
     return a+b;
 }
-document.getElementById("result").textContent=add(2,3)
-const greet=document.getElementById("test");
+document.getElementById("val").textContent=add(2,3)
 const age = document.getElementById("age");
 const button = document.getElementById("click");
-const result = document.getElementById("eligible");
-greet.textContent="hello r";
+const result = document.getElementById("result");
 button.addEventListener("click",()=>{
-const adult= age>=18 ? "ur are eligble":"ur no eligible"
-result.textContent(adult)
+const adult= age.value>=18 ?"ur are eligble":"ur not eligible"
+result.textContent=adult;
+});
