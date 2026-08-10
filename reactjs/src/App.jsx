16b639b23cf2d1d8e5b4013b3d5e5387403a9573
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-11 01:25:58 +0530

    component

---
+++ reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+function Greet(){
+    return(
+        <>
+        <h1>hello</h1>
+        </>
+    );
+}
+export default function infos(){
+    return(
+        <>
+        <Greet/>
+        <Greet/>
+        <Greet/>
+        </>
+    );
+}
