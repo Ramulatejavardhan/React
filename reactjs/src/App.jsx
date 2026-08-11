72a7efcfd92f42f46dfd42ef1cd534cec95c1677
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-11 14:53:54 +0530

    image accessing various methods

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,15 +1,21 @@
+import React from "react";
+import hanuman from "./hanuman.jpg";
 function Greet(){
     return(
         <>
         <h1>hello</h1>
+        <a href="https://www.google.com/search?q=hanuman+pics&oq=hanuman+pics&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDMwNzZqMGo3qAIAsAIA&sourceid=chrome&source=chrome.ob&ie=UTF-8&sei=ZNt6asOAGrmYseMP34T2gAo">
+        <img src={hanuman} alt="hanumanji pic" width="250px" height="auto"/>
+        </a>
+        {/*without public folder we have to import the image first and we have to use in src={hanuman}*/}
+        <img src="/hanuman1.jpg" alt="hanumanji1"/>
+        {/* we can access the image by giving that image to public folder without importing the image. */}
         </>
     );
 }
 export default function infos(){
     return(
         <>
-        <Greet/>
-        <Greet/>
         <Greet/>
         </>
     );
