90eb8f0351b084c9c013d38dfc29718175351c99
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-12 20:49:40 +0530

    image accessing various methods

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,22 +1,10 @@
 import React from "react";
-import hanuman from "./hanuman.jpg";
-function Greet(){
+import Image from "./img.jsx";
+function App(){
     return(
         <>
-        <h1>hello</h1>
-        <a href="https://www.google.com/search?q=hanuman+pics&oq=hanuman+pics&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDMwNzZqMGo3qAIAsAIA&sourceid=chrome&source=chrome.ob&ie=UTF-8&sei=ZNt6asOAGrmYseMP34T2gAo">
-        <img src={hanuman} alt="hanumanji pic" width="250px" height="auto"/>
-        </a>
-        {/*without public folder we have to import the image first and we have to use in src={hanuman}*/}
-        <img src="/hanuman1.jpg" alt="hanumanji1"/>
-        {/* we can access the image by giving that image to public folder without importing the image. */}
+        <Image/>
         </>
     );
 }
-export default function infos(){
-    return(
-        <>
-        <Greet/>
-        </>
-    );
-}
+export default App;
---
+++ reactjs/src/img.jsx
@@ -0,0 +1,16 @@
+import hanuman from "./hanuman.jpg"
+function Image(){
+    const name="tej"
+    return(
+        <>
+        <h1>JAI SREE RAM🙏</h1>
+        <img src={hanuman} alt="hanumanjipic" width="250px"/>
+        {/*without public folder we have to import the image first and we have to use in src={hanuman}*/}
+        <br/>
+        <img src="hanuman1.jpg" alt="hanumanji" width="220px"/>
+        {/* we can access the image by giving that image to public folder without importing the image. */}
+        <h1>Hello{name}</h1>
+        </>
+    );
+}
+export default Image;
