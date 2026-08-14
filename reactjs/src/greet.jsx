dc8f8fd3c496feb5fbbe04e043418143b85b1827
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-14 15:41:08 +0530

    props accessing

--- reactjs/src/greet.jsx
+++ reactjs/src/greet.jsx
@@ -2,6 +2,7 @@ export default function greet(props){
     return(
         <>
             <h1>hello{props.name}</h1>
+            <h1>are u {props.age} yrs old</h1>
         </>
     );                
 }
