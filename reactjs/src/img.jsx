c02c75a38b0327d0e1aa8c3fa3e542e255d135ca
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-15 23:38:18 +0530

    props accessing

--- reactjs/src/img.jsx
+++ reactjs/src/img.jsx
@@ -9,8 +9,8 @@ function Image(){
         <br/>
         <img src="hanuman1.jpg" alt="hanumanji" width="220px"/>
         {/* we can access the image by giving that image to public folder without importing the image. */}
-         <h1>Hello{name}</h1> {/* we can access the element by {} to use the javascript} */}
-        <h1>Hello{name}</h1>
+         <h1>Hello JAI SREE RAM🙏 {name}</h1> {/* we can access the element by {} to use the javascript} */}
+        <h1>Hello JAI{name}</h1>
         </>
     );
 }
