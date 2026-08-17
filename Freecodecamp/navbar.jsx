6bfdf7ab2b167aeb8970f728670a3862c875cc04
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-18 01:04:13 +0530

    navbar creation

---
+++ Freecodecamp/navbar.jsx
@@ -0,0 +1,19 @@
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="nav-item">
+          <a href="#">Dashboard</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Widgets</a>
+        </li>
+        <li className="nav-item">
+          <button aria-expanded="false">Apps</button>        
+          <ul className="sub-menu" aria-label="Apps">            
+          </ul>      
+        </li>
+      </ul>
+    </nav>
+  );
+};
