0c8a1855483df27a33b1cc72412edf4c9c2fd8e1
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-21 12:25:20 +0530

    rendering

--- reactjs/src/components/rendering.jsx
+++ reactjs/src/components/rendering.jsx
@@ -1,7 +1,7 @@
 export default function Rendering(){
-    const age=19;
+    const age=17;
     if (age>=18){
         return <h1>Ur {age} yrs adult</h1>
     }
-    return <h1>Ur not adult</h1>
+    return <h1>Ur still {age}yrs not an adult</h1>
 }
