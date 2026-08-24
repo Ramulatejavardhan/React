f381d831f573ec5d2933fabe0aba9da49ee59709
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-25 00:11:27 +0530

    mapinreact

--- reactjs/src/components/map.jsx
+++ reactjs/src/components/map.jsx
@@ -1,10 +1,14 @@
 const names = ["R", "Tej", "Ram"];
+const ages=[19,20,21];
 
 export default function Maps() {
     return (
         <>
             {names.map((name) => (
                 <p key={name}>Hello {name}</p>
+            ))}
+            {ages.map((age) =>(
+                <p>my age is {age}</p>
             ))}
         </>
     );
