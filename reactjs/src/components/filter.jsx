27d3c6b01cf9ed52bc2dc5b5ba4040d36b1d162b
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-26 01:47:09 +0530

    map(filter)

--- reactjs/src/components/filter.jsx
+++ reactjs/src/components/filter.jsx
@@ -1,11 +1,18 @@
 const students=[
-        { id:1,name:"Jai",age:20},
-        { id:2,name:"Sree",age:21},
-        { id:3,name:"Ram",age:21}
+    {id:1,name:"Jai",age:19},
+    {id:2,name:"Sree",age:20},
+    {id:3,name:"Ram",age:19}
 ]
-    const adult=students.filter(
-        (student) =>student.age>20
+const adults=students.filter(
+    (student) => student.age>19
 );
 export default function Filter(){
-    return <h4>{adult}</h4>
+    return(
+        {adults.map((adult) =>(
+        <div key={adult.id}>
+            <p>This are the adult persons names</p>
+            <h4>{adult.name}</h4>
+        </div>
+    ))}
+    );
 }
