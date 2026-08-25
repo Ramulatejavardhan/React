100788155b09c27a7cd6569e77eaf16b7c7c6173
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-26 00:59:12 +0530

    mapinreact

---
+++ reactjs/src/components/filter.jsx
@@ -0,0 +1,10 @@
+export default function filter(){
+    const students=[
+        { id:1,name:"Jai",age:20},
+        { id:2,name:"Sree",age:21},
+        { id:3,name:"Ram",age:22}
+    ]
+    const adult=students.filter(
+        (student) =>student.age>=18
+    );
+}
