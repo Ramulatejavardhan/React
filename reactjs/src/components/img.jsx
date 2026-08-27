e29a2e1baa302deda22a5cc5472683090c19bfbe
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-27 20:59:56 +0530

    cssupdating

--- reactjs/src/components/img.jsx
+++ reactjs/src/components/img.jsx
@@ -4,10 +4,10 @@ function Image(){
     return(
         <>
         <h1>JAI SREE RAM🙏</h1>
-        <img src={hanuman} alt="hanumanjipic" width="250px"/>
+        <img class="image" src={hanuman} alt="hanumanjipic" width="250px"/>
         {/*without public folder we have to import the image first and we have to use in src={hanuman}*/}
         <br/>
-        <img src="hanuman1.jpg" alt="hanumanji" width="220px"/>
+        <img class="image" src="hanuman1.jpg" alt="hanumanji" width="220px"/>
         {/* we can access the image by giving that image to public folder without importing the image. */}
          <h1>Hello JAI SREE RAM🙏 {name}</h1> {/* we can access the element by {} to use the javascript} */}
         <h1>Hello JAI SREE RAM🙏{name}</h1>
