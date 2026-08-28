a7f1766b00c6c207e879b9e95256be4e0a5af6bd
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-29 00:18:37 +0530

    usestate

--- reactjs/src/components/Usestate.jsx
+++ reactjs/src/components/Usestate.jsx
@@ -1,1 +1,10 @@
 import { useState } from "react";
+export default function useState(){
+    const[count,setcount] =useState(0);
+    return(
+        <>
+            <h1>Count</h1>
+            <button onClick={()=> setcount(count+1)}>➕</button>
+        </>
+    );
+}
